03c8060eabeb598e7747d7657c416c7f4df6ece0
Bay <rlbyqf@rc02xcs213.managed.mst.edu>
2017-02-02 12:02:37 -0600

    division is done

--- TDDcalc.cpp
+++ TDDcalc.cpp
@@ -22,7 +22,10 @@ int multiplication(int x , int y)
 	return x*y;
 }
 
-
+int division(int x , int y)
+{
+	return x/y;
+}
 
 
 
@@ -31,9 +34,14 @@ int main()
 	int num;
 	num = addition(4,5);
 	cout << num << endl;
+	
 	num = substraction(4,5);
 	cout << num << endl;
+	
 	num = multiplication(4,5);
+	cout << num << endl;
+	
+	num = division(10,5);
 	cout << num << endl;
 	
 	return 0;
