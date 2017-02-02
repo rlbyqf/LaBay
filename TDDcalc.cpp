5f22a2a2d2658253b5f80d89633792a45de220e5
Bay <rlbyqf@rc02xcs213.managed.mst.edu>
2017-02-02 11:47:58 -0600

    Addition has been added

---
+++ TDDcalc.cpp
@@ -0,0 +1,32 @@
+//Ryan Bay
+//Austin La
+
+
+#include <iostream>
+using namespace std;
+
+int addition(int x,int y)
+{
+	return x + y;
+	
+}
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	
+	cout << addition(4,5);
+	
+	
+	
+	return 0;
+}
+
