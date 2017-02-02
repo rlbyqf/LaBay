b3f73d6e14f1e554037d91e86ab13b5a33901cba
Bay <rlbyqf@rc02xcs213.managed.mst.edu>
2017-02-02 11:58:52 -0600

    multiplication is done

--- TDDcalc.cpp
+++ TDDcalc.cpp
@@ -17,7 +17,10 @@ int substraction(int x, int y)
 }
 
 
-
+int multiplication(int x , int y)
+{
+	return x*y;
+}
 
 
 
@@ -27,9 +30,11 @@ int main()
 {
 	int num;
 	num = addition(4,5);
-	cout << num;
+	cout << num << endl;
 	num = substraction(4,5);
-	
+	cout << num << endl;
+	num = multiplication(4,5);
+	cout << num << endl;
 	
 	return 0;
 }
